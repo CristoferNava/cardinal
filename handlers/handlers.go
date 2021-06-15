f9f12e4ee1168280633218febd05d896226d698f
CristoferNava <cj.carmonanava@ugto.mx>
2021-06-15 17:54:00 -0500

    URI for change profile was added

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,6 +19,7 @@ func Handle() {
 	router.HandleFunc("/sign-up", mw.CheckDB(routers.SignUp)).Methods("POST")
 	router.HandleFunc("/log-in", mw.CheckDB(routers.LogIn)).Methods("POST")
 	router.HandleFunc("/show-profile", mw.CheckDB(mw.ValidateJWT(routers.ShowProfile))).Methods("GET")
+	router.HandleFunc("/change-profile", mw.CheckDB(mw.ValidateJWT(routers.ChangeProfile))).Methods("PUT")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
